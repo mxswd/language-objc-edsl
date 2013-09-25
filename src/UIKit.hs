db0af1e2146a0ea33bee7240b22116c21440e47d
Maxwell Swadling <maxwellswadling@gmail.com>
2013-09-25 14:44:08 +1000

    UIKit imports

--- src/UIKit.hs
+++ src/UIKit.hs
@@ -1,3 +1,15 @@
+{-# LANGUAGE FlexibleInstances, DataKinds, GADTs, KindSignatures, ExistentialQuantification, QuasiQuotes, TemplateHaskell, StandaloneDeriving, TypeOperators, ImpredicativeTypes, TypeFamilies, ScopedTypeVariables, QuasiQuotes, OverloadedStrings #-}
+
+module UIKit where
+
+import Language.C.Quote.ObjC
+import Language.C hiding (Func)
+import Data.Loc
+import Data.String
+import Backend
+import OM
+
+import System.IO.Unsafe
 
 -- newArrayProp :: OM (Bind Global TTrue (NSArray t))
 -- newArrayProp = do
